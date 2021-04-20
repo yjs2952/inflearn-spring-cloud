09cb64f3ae7b263dde997efb17dfcd1207146e29
yjs2952 <yjs2952@naver.com>
2021-04-20 23:15:40 +0900

    서비스명 변경

--- discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryserviceApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryserviceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
--- userservice/src/main/java/com/example/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserserviceApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserserviceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 }
