431ecd01f939e85993a013032a68de68c7cbeb82
yjs2952 <yjs2952@naver.com>
2021-05-07 21:53:38 +0900

    Users Microservice - Spring Security 연동

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 2. 터미널에서 mvn (또는 프로젝트 루트 경로에서 ./mvnw) spring-boot:run -Dspring-boot-run.jvmArguments='-Dserver.port=9001'
 3. mvn clean compile package   
    java -jar -Dserver.port=9001 **./target/user-service-0.0.1-SNAPSHOT.jar (jar 파일 경로)** 
+   
+### Users Microservice - Security
+* http.headers().frameOptions().disable();
+    - h2-console 접근하기 위한 옵션
+* BCryptPasswordEncoder
+    - Password 를 해싱하기 위해 Bcrypt 알고리즘 사용
+    - 랜덤 Salt 를 부여하여 여러번 Hash 를 적용한 암호화 방식
---
+++ userservice/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,32 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.headers().frameOptions().disable();    // 이거 안하면 h2-console 접근 못함
+    }
+}
