eb6e77a001e27114ba19b956c85880cac8648e7c
yjs2952 <yjs2952@naver.com>
2021-05-14 23:43:51 +0900

    Users Microservice - Routes 테스트

--- userservice/src/main/java/com/example/userservice/controller/UserController.java
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/user-service")
+@RequestMapping("/")
 public class UserController {
 
     private final Environment env;
--- userservice/src/main/java/com/example/userservice/security/WebSecurity.java
+++ userservice/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -2,7 +2,6 @@ package com.example.userservice.security;
 
 import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,17 +23,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 //
 //    }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("192.168.123.103")
+                .hasIpAddress("192.168.123.105")
                 .and()
                 .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();    // 이거 안하면 h2-console 접근 못함
