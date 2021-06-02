cdc698a80e505c6d8a3586a051883ed86a548718
yjs2952 <yjs2952@naver.com>
2021-06-02 22:54:33 +0900

    Users Microservice 에서 Spring Cloud Config 연동

--- userservice/src/main/java/com/example/userservice/security/WebSecurity.java
+++ userservice/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -27,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress(env.getProperty("spring.cloud.client.ip-address"))
                 .and()
